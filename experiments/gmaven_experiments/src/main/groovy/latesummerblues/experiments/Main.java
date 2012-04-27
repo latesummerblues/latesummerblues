8f4dba560f0c7aca52dcf61e934352b904add773
latesummerblues@gmail.com <latesummerblues@gmail.com>
2012-04-27 21:53:54 +0300

    experiments

---
+++ experiments/gmaven_experiments/src/main/groovy/latesummerblues/experiments/Main.java
@@ -0,0 +1,14 @@
+package latesummerblues.experiments;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		new Example1().show();
+	}
+
+}
